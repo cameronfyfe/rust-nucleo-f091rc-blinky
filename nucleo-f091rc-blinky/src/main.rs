602f69d51f1ecb1865d056ad4bb8f81030e35717
cameornf <cameron.j.fyfe@gmail.com>
2018-12-16 13:26:01 -0700

    Initial commit of blinky main

--- nucleo-f091rc-blinky/src/main.rs
+++ nucleo-f091rc-blinky/src/main.rs
@@ -1,20 +1,39 @@
-#![no_std]
 #![no_main]
+#![no_std]
 
-// pick a panicking behavior
-extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
-// extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
+extern crate stm32f0;
+extern crate panic_halt;
 
+use stm32f0::stm32f0x1;
 use cortex_m::asm;
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    // Get references to STM32F091RC rcc and gpioa peripherals
+    let per = stm32f0x1::Peripherals::take().unwrap();
+    let rcc = &per.RCC;
+    let gpioa = &per.GPIOA;
+
+    // RCC GPIOA Clock Enable
+    rcc.ahbenr.write(|w| w.iopaen().set_bit());
+
+    // Set PA5 as digital output
+    gpioa.moder.write(|w| w.moder5().bits(1));
+
+    // LED toggle bit
+    let mut led_toggle = false;
 
     loop {
-        // your code goes here
+
+        // Flip toggle bit and write to output data bit
+        led_toggle = !led_toggle;
+        gpioa.odr.write(|w| w.odr5().bit(led_toggle));
+
+        // Wait for 5000 nops
+        for _i in 0..5000 {
+            asm::nop()
+        }
     }
 }
